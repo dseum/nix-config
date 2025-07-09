5dbaa4b5d89a5df305e24773c6eea5af2dd0bab3
dseum <me@denniseum.com>
2025-07-09 19:36:07 +0400

    fix: ghostty patch for kernel 6.15.4

--- module/nixos/home-manager.nix
+++ module/nixos/home-manager.nix
@@ -23,6 +23,13 @@ in
         imports = [
           ../shared/home-manager.nix
         ];
+        programs.ghostty.package = pkgs.ghostty.overrideAttrs (_: {
+          preBuild = ''
+            shopt -s globstar
+            sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+            shopt -u globstar
+          '';
+        });
         home = {
           file = shared-files // import ./files.nix { inherit config user; };
           homeDirectory = "/home/${user}";
