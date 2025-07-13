c5987be118217dd1c41cbee70dd0966bd15ddeca
dseum <me@denniseum.com>
2025-07-13 22:30:48 +0400

    feat: improved prompt

--- module/shared/home-manager.nix
+++ module/shared/home-manager.nix
@@ -26,9 +26,7 @@
         fish_greeting = "";
         fish_mode_prompt = "";
         fish_prompt = ''
-          set -l cols (tput cols)
-          echo (string repeat -n "$cols" "─")
-          echo -n (set_color cyan -o)(prompt_pwd)\n(set_color normal)(set_color black -b white)" $(whoami) "(set_color normal)(set_color white)" "(set_color normal)
+          echo -n (string repeat -n "$(tput cols)" "─")\n(set_color cyan -o)(prompt_pwd)\n(set_color normal)(set_color black -b white)" $(whoami) "(set_color normal)(set_color white)" "(set_color normal)
         '';
       };
       interactiveShellInit = ''
