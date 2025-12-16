2651f177feddaf82c06f73e15d691ea945976897
dseum <me@denniseum.com>
2025-12-16 18:46:28 -0500

    feat: claude-code shift enter keybind

--- module/shared/home-manager.nix
+++ module/shared/home-manager.nix
@@ -112,6 +112,7 @@
         keybind = [
           "global:cmd+backquote=toggle_quick_terminal"
           "global:shift+alt+e=new_window"
+          "shift+enter=text:\x1b\r"
         ];
       };
     };
