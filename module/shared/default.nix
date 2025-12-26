96b662b36dbdf6559e4ac98a30fb23e7ea4eb668
dseum <me@denniseum.com>
2025-12-26 14:09:13 +0400

    chore: update spotify

--- module/shared/default.nix
+++ module/shared/default.nix
@@ -11,8 +11,8 @@
           src =
             if (prev.stdenv.isDarwin && prev.stdenv.isAarch64) then
               prev.fetchurl {
-                url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-                hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
+                url = "https://web.archive.org/web/20251218095912/http://download.scdn.co/SpotifyARM64.dmg";
+                hash = "sha256-Jx7pmRn1x1qV38Ku6KE6m4gABnM6FlfYis20O+zU1Y8=";
               }
             else
               oldAttrs.src;
