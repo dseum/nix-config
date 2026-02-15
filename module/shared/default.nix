fbd3d270cdae7f4a1ece6fbd20c14ba779aaac7f
dseum <me@denniseum.com>
2026-02-14 22:57:29 -0800

    chore: update flake

--- module/shared/default.nix
+++ module/shared/default.nix
@@ -14,7 +14,7 @@
           src =
             if (prev.stdenv.isDarwin && prev.stdenv.isAarch64) then
               prev.fetchurl {
-                url = "https://web.archive.org/web/20260125183514/http://download.scdn.co/SpotifyARM64.dmg";
+                url = "https://web.archive.org/web/20260213045156/http://download.scdn.co/SpotifyARM64.dmg";
                 hash = "sha256-cslyAkpAXsVvIfx7tsDpDxnSjidH2uHCeFBq3pXFaMo=";
               }
             else
