beb907c6d6c5225fd563226316d5294cc1ff74bd
dseum <me@denniseum.com>
2026-07-09 11:35:57 -0700

    chore: update flake

--- module/shared/packages.nix
+++ module/shared/packages.nix
@@ -19,9 +19,9 @@
   pkgs.ninja
   pkgs.nixd
   pkgs.nixfmt
-  pkgs.nodejs_22
+  pkgs.nodejs_26
   pkgs.opam
-  (pkgs.pnpm_11.override { nodejs-slim = pkgs.nodejs-slim_22; })
+  (pkgs.pnpm_11.override { nodejs-slim = pkgs.nodejs-slim_26; })
   pkgs.python313
   pkgs.qemu
   pkgs.R
