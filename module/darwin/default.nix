8ed86f74d6eb296faed2b2b8c79d8bc2f2dd2acc
dseum <me@denniseum.com>
2026-08-26 00:34:58 -0700

    fix: remove linux builder

--- module/darwin/default.nix
+++ module/darwin/default.nix
@@ -50,28 +50,6 @@ in
       Hour = 2;
       Minute = 30;
     };
-    linux-builder = {
-      enable = true;
-      config = {
-        virtualisation = {
-          cores = 8;
-          darwin-builder = {
-            diskSize = 80 * 1024;
-            memorySize = 16 * 1024;
-          };
-        };
-      };
-      maxJobs = 8;
-      supportedFeatures = [
-        "kvm"
-        "benchmark"
-        "big-parallel"
-        "nixos-test"
-      ];
-      systems = [
-        "aarch64-linux"
-      ];
-    };
   };
   nix-homebrew = {
     inherit user;
