6e6dd461de447c7369e19235aeb81ff36ba93a7a
dseum <me@denniseum.com>
2026-09-10 18:23:13 -0700

    feat(nvim): copy relative and absolute path keymaps

--- module/shared/config/nvim/lua/config/keymaps.lua
+++ module/shared/config/nvim/lua/config/keymaps.lua
@@ -12,6 +12,27 @@ vim.keymap.set("n", "<c-u>", "<c-u>zz")
 vim.keymap.set("x", "p", [["_dP]])                 -- Puts without filling "" register
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- Yanks into "+ register
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+local function copy_file_path(modifier)
+  local path = vim.api.nvim_buf_get_name(0)
+  if path == "" then
+    vim.notify("Buffer has no file", vim.log.levels.WARN)
+    return
+  end
+
+  path = vim.fn.fnamemodify(path, modifier)
+  vim.fn.setreg("+", path)
+  vim.notify("Copied " .. path)
+end
+
+vim.keymap.set("n", "<leader>fr", function()
+  copy_file_path(":.")
+end, { desc = "Copy relative file path" })
+
+vim.keymap.set("n", "<leader>fa", function()
+  copy_file_path(":p")
+end, { desc = "Copy absolute file path" })
+
 vim.keymap.set("t", "<c-w>j", "<c-\\><c-n><c-w>j")
 vim.keymap.set("t", "<c-w>k", "<c-\\><c-n><c-w>k")
 vim.keymap.set("t", "<c-w>h", "<c-\\><c-n><c-w>h")
